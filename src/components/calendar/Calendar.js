11555c7c50c5fba443cbdb2e59f19b392593b438
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2024-09-06 20:27:02 +0700

    calender 3

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -41,7 +41,7 @@ const Calendar = () => {
                             </div>
                         </div>
                     </div>
-                    <div class="col-5">
+                    <div class="col-6">
                         <h3 class="text-uppercase"><strong>Qatar Airways Grand Prix of Qatar</strong></h3>
                         <div class="mt-2 d-flex justify-content-left">
                             <div class="location_flag d-flex">
@@ -50,21 +50,76 @@ const Calendar = () => {
                             <div class="calendar-listing__location-track-name mt-1">Lusail International Circuit</div>
                         </div>
                     </div>
-                    <div class="col-2 d-flex justify-content-center align-items-center bg-dark">
-                        {/* <img class="calendar-listing__layout" src="https://photos.motogp.com/events-admin/b/1/b1cd25b2-1658-4c3b-bd3f-1a7796f7cd27/simple/qat2.svg" alt="Lusail International Circuit track" loading="lazy"></img> */}
-                    </div>
+                    {/* <div class="col-2 d-flex justify-content-center align-items-center bg-dark">
+                        <img class="calendar-listing__layout" src="https://photos.motogp.com/events-admin/b/1/b1cd25b2-1658-4c3b-bd3f-1a7796f7cd27/simple/qat2.svg" alt="Lusail International Circuit track" loading="lazy"></img>
+                    </div> */}
 
+                    <div class="col-3 d-flex justify-content-start align-items-center bg-dark">
+                        <div class="d-flex justify-content-start align-items-center">
+                            <img class="calendar-listing__layout" src="https://photos.motogp.com/events-admin/b/1/b1cd25b2-1658-4c3b-bd3f-1a7796f7cd27/simple/qat2.svg" alt="Lusail International Circuit track" loading="lazy"></img>
+                        </div>
+                        <div class="fade-effect">
+                            <img src={motor1} alt="Event Image" class="ms-3 shadow-image"></img>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-striped mb-5 mt-3">
+                    <div class="day col-2">
+                        <div class="row justify-content-center">
+                            <div class="col text-center">
+                                <div class="date-component">
+                                    <h1 class="display-4">3</h1>
+                                    <span class="month">MAR</span>
+                                </div>
+                            </div>
+                            <div class="col text-center">
+                                <div class="date-component">
+                                    <h1 class="display-4">-</h1>
+                                </div>
+                            </div>
+                            <div class="col text-center">
+                                <div class="date-component">
+                                    <h1 class="display-4">10</h1>
+                                    <span class="month">MAR</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <h3 class="text-uppercase"><strong>Qatar Airways Grand Prix of Qatar</strong></h3>
+                        <div class="mt-2 d-flex justify-content-left">
+                            <div class="location_flag d-flex">
+                                <img class="calendar-listing_flag" src="https://static-files.motogp.pulselive.com/assets/flags/qa.svg" alt="ES flag" loading="lazy"></img>
+                            </div>
+                            <div class="calendar-listing__location-track-name mt-1">Lusail International Circuit</div>
+                        </div>
+                    </div>
+                    {/* <div class="col-2 d-flex justify-content-center align-items-center bg-dark">
+                        <img class="calendar-listing__layout" src="https://photos.motogp.com/events-admin/b/1/b1cd25b2-1658-4c3b-bd3f-1a7796f7cd27/simple/qat2.svg" alt="Lusail International Circuit track" loading="lazy"></img>
+                    </div> */}
+
+                    <div class="col-3 d-flex justify-content-start align-items-center bg-dark">
+                        <div class="d-flex justify-content-start align-items-center">
+                            <img class="calendar-listing__layout" src="https://photos.motogp.com/events-admin/b/1/b1cd25b2-1658-4c3b-bd3f-1a7796f7cd27/simple/qat2.svg" alt="Lusail International Circuit track" loading="lazy"></img>
+                        </div>
+                        <div class="fade-effect">
+                            <img src={motor1} alt="Event Image" class="ms-3 shadow-image"></img>
+                        </div>
+                    </div>
+                </div>
+                
                     <div class="col-3">
                         <div class="fade-effect">
                             <img src={motor1} alt="Event Image" class=" ms-2 shadow-image"></img>
                         </div>
                     </div>
                 </div>
-                
-                
 
 
-               
+
+
+
+
 
                 <div class="row row-striped mb-5 mt-3">
                     <div class="col-2 text-right">
@@ -80,7 +135,7 @@ const Calendar = () => {
                         </ul>
                     </div>
                 </div>
-            </div>
+            
         </>
     );
 };
