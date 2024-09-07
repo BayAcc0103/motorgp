18586d779ee997b243a80ce9320e6cff1f96f302
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2024-09-07 22:51:02 +0700

    update dropdown

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Navbar from './components/navbar/Navbar'
 import Homepage from './components/news/Homepage'
 import LoginForm from './components/loginform/Loginform';
 import Calendar from './components/calendar/Calendar';
+<<<<<<< Updated upstream
 import AddEvent from './components/addevent/AddEvent';
+=======
+import Result from './components/result/Result';
+import Standing from './components/standing/Standing';
+
+>>>>>>> Stashed changes
 const App = () => {
   return (
     <Router>
@@ -13,7 +19,12 @@ const App = () => {
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/calendar" element={<Calendar/>}/>
+<<<<<<< Updated upstream
           <Route path="/addevent" element={<AddEvent/>}/>
+=======
+          <Route path="/result" element={<Result/>}/>
+          <Route path="/standing" element={<Standing/>}/>
+>>>>>>> Stashed changes
         </Routes>
     </Router>
   )
