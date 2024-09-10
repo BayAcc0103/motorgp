084fca80de9d6325653218a201007ecc764c3c16
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2024-09-10 23:08:16 +0700

    Result & Standing

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,84 +1,102 @@
 import React from 'react';
-import "./Result.css"
+import styles from "./Result.module.css";
 const Result = () => {
 
     return (
         <>
             <div>
-                <div class="primary-filter">
-                    <div class="primary-filter__filter-container">
-                        <div class="primary-filter__filter-label">Year</div>
-                        <select class="primary-filter__filter-select primary-filter__filter-select--year">
-                            <option value="2024">2024</option>
-                            <option value="2023">2023</option>
-                        </select>
+                <div class={`z-1 position-relative`}>
+                    <div class={`d-flex position-relative ${styles.primary_filter}`}>
+                        <div class={`d-flex position-relative align-items-center ${styles.filter_container}`}>
+                            <div class={`${styles.filter_label}`}>Year</div>
+                            <select class={`${styles.filter_select} ${styles.custom_select}`}>
+                                <option class={`text-dark ${styles.custom_option}`} value="2024">2024</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2023">2023</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2022">2022</option>
+                            </select>
+                        </div>
+                        <div class={`d-flex position-relative align-items-center ${styles.filter_container}`}>
+                            <div class={`${styles.filter_label}`}>Year</div>
+                            <select class={`${styles.filter_select} ${styles.custom_select}`}>
+                                <option class={`text-dark ${styles.custom_option}`} value="2024">2024</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2023">2023</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2022">2022</option>
+                            </select>
+                        </div>
+                        <div class={`d-flex position-relative align-items-center ${styles.filter_container}`}>
+                            <div class={`${styles.filter_label}`}>Year</div>
+                            <select class={`${styles.filter_select} ${styles.custom_select}`}>
+                                <option class={`text-dark ${styles.custom_option}`} value="2024">2024</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2023">2023</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2022">2022</option>
+                            </select>
+                        </div>
+                        <div class={`d-flex position-relative align-items-center ${styles.filter_container}`}>
+                            <div class={`${styles.filter_label}`}>Year</div>
+                            <select class={`${styles.filter_select} ${styles.custom_select}`}>
+                                <option class={`text-dark ${styles.custom_option}`} value="2024">2024</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2023">2023</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2022">2022</option>
+                            </select>
+                        </div>
                     </div>
-                    <div class="primary-filter__filter-container">
-                        <div class="primary-filter__filter-label">Event</div>
-                        <select class="primary-filter__filter-select primary-filter__filter-select--year">
-                            <option value="2024">2024</option>
-                            <option value="2023">2023</option>
-                        </select>
+                </div>
+                <div class={`${styles.results__hero_container}`}>
+                    <div class={`${styles.results__hero_image}`}>
+                        <img alt="Misano MotoGP™ Official Test" src="https://photos.motogp.com/2024/events/background/RSM.png" class={`${styles.results__hero_image_img}`}></img>
                     </div>
-                    <div class="primary-filter__filter-container">
-                        <div class="primary-filter__filter-label">Category</div>
-                        <select class="primary-filter__filter-select primary-filter__filter-select--year">
-                            <option value="2024">2024</option>
-                            <option value="2023">2023</option>
-                        </select>
-                    </div>
-                    <div class="primary-filter__filter-container">
-                        <div class="primary-filter__filter-label">Session</div>
-                        <select class="primary-filter__filter-select primary-filter__filter-select--year">
-                            <option value="2024">2024</option>
-                            <option value="2023">2023</option>
-                        </select>
+                    <div class={`${styles.results__hero_text}`}>Misano MotoGP™ Official Test 2024</div>
+                    <div class={`${styles.results__hero_details_container}`}>
+                        <img src="https://static-files.motogp.pulselive.com/assets/flags/it.svg" alt="IT flag" class={`${styles.results__hero_details_flag}`}></img>
+                        <div class={`${styles.results__hero_details_location}`}>Misano World Circuit Marco Simoncelli</div>
+                        <div class={`${styles.results__hero_details_dates}`}>09 Sep</div>
                     </div>
                 </div>
 
-                <section class="results-table">
-                    <table class="results-table__table">
+
+                <section class={`${styles.results_table}`}>
+                    <table class={`${styles.results_table__table}`}>
                         <thead>
                             <tr>
-                                <th class="results-table__header-cell results-table__header-cell--pos u-hide-tablet">pos.</th>
-                                <th class="results-table__header-cell results-table__header-cell--points u-hide-tablet">pts</th>
-                                <th class="results-table__header-cell results-table__header-cell--rider">rider</th>
-                                <th class="results-table__header-cell results-table__header-cell--team u-hide-tablet">Team</th>
-                                <th class="results-table__header-cell results-table__header-cell--time">time / gap</th>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__pos}`}>pos.</th>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__points}`}>pts</th>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__rider}`}>rider</th>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__team}`}>Team</th>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__time}`}>time / gap</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr class="results-table__body-row">
-                                <td class="results-table__body-cell results-table__body-cell--pos u-hide-tablet">1</td>
-                                <td class="results-table__body-cell results-table__body-cell--points u-hide-tablet">25</td>
-                                <td class="results-table__body-cell results-table__body-cell--rider">
-                                    <div class="results-table__rider-details">
-                                        <div class="rider-image-container">
+                        <tbody class={`${styles.results_table__tbody}`}>
+                            <tr class={`${styles.results_table__body_row}`}>
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__pos} u-hide-tablet`}>1</td>
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__pos} u-hide-tablet`}>25</td>
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__pos}`}>
+                                    <div class={`${styles.results_table__rider_details}`}>
+                                        <div class={`${styles.rider_image_container}`}>
                                             <div class="rider-image">
-                                                <div class="js-lazy-load u-observed lazy-image-wrapper ">
+                                                <div class={`${styles.u_observed}`}>
                                                     <img src="https://resources.motogp.pulselive.com/photo-resources/2024/02/19/986b0e12-1db0-49d8-ae13-fd556286237a/93_Marc_MarquezFullbodyGresini.png?height=300&amp;width=200"
                                                         alt="rider-bio_marcmarquez" loading="lazy"
                                                         class="img undefined picture__img "></img>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="results-table__rider-name-wrapper u-hide-tablet">
-                                            <div class="results-table__rider-name">
-                                                <span class="results-table__body-cell results-table__body-cell--number">93</span>
-                                                <span class="results-table__body-cell results-table__body-cell--full-name">
+                                        <div class={`${styles.results_table__rider_name_wrapper} u-hide-tablet`}>
+                                            <div class={`${styles.results_table__rider_name}`}>
+                                                <span class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__number}`}>93</span>
+                                                <span class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__full_name}`}>
                                                     <a href="/en/riders/marc-marquez/23e50438-a657-4fb0-a190-3262b5472f29"
-                                                        title="label.standings.viewRiderProfile" class="results-table__rider-link">
-                                                        <span class="results-table__first-name">M. </span> Marquez</a></span>
+                                                        title="label.standings.viewRiderProfile" class={`${styles.results_table__rider_link}`}>
+                                                        <span class={`${styles.results_table__first_name}`}>M. </span> Marquez</a></span>
                                             </div>
                                             <img src="https://static-files.motogp.pulselive.com/assets/flags/es.svg" alt="ES flag"
-                                                class="results-table__body-cell-flag"></img>
+                                                class={`${styles.results_table__body_cell_flag}`}></img>
                                         </div>
                                     </div>
                                 </td>
-                                <td class="results-table__body-cell results-table__body-cell--team u-hide-tablet">Gresini Racing
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__team} u-hide-tablet`}>Gresini Racing
                                     MotoGP
                                 </td>
-                                <td class="results-table__body-cell results-table__body-cell--time"><span>41:47.082</span></td>
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__time}`}><span>41:47.082</span></td>
                             </tr>
                         </tbody>
                     </table>
--- src/components/standing/Standing.js
+++ src/components/standing/Standing.js
@@ -1,30 +1,103 @@
 import React from 'react';
-import "./Standing.css"
+import styles from "./Standing.module.css";
 const Standing = () => {
     return (
-        <div class="primary-filter">
-            <div class="primary-filter__filter-container">
-                <div class="primary-filter__filter-label">Year</div>
-                <select class="primary-filter__filter-select primary-filter__filter-select--year">
-                    <option value="2024">2024</option>
-                    <option value="2023">2023</option>
-                </select>
+        <>
+            <div>
+                <div class={`z-1 position-relative`}>
+                    <div class={`d-flex position-relative ${styles.primary_filter}`}>
+                        <div class={`d-flex position-relative align-items-center ${styles.filter_container}`}>
+                            <div class={`${styles.filter_label}`}>Year</div>
+                            <select class={`${styles.filter_select} ${styles.custom_select}`}>
+                                <option class={`text-dark ${styles.custom_option}`} value="2024">2024</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2023">2023</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2022">2022</option>
+                            </select>
+                        </div>
+                        <div class={`d-flex position-relative align-items-center ${styles.filter_container}`}>
+                            <div class={`${styles.filter_label}`}>Year</div>
+                            <select class={`${styles.filter_select} ${styles.custom_select}`}>
+                                <option class={`text-dark ${styles.custom_option}`} value="2024">2024</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2023">2023</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2022">2022</option>
+                            </select>
+                        </div>
+                        <div class={`d-flex position-relative align-items-center ${styles.filter_container}`}>
+                            <div class={`${styles.filter_label}`}>Year</div>
+                            <select class={`${styles.filter_select} ${styles.custom_select}`}>
+                                <option class={`text-dark ${styles.custom_option}`} value="2024">2024</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2023">2023</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2022">2022</option>
+                            </select>
+                        </div>
+                        <div class={`d-flex position-relative align-items-center ${styles.filter_container}`}>
+                            <div class={`${styles.filter_label}`}>Year</div>
+                            <select class={`${styles.filter_select} ${styles.custom_select}`}>
+                                <option class={`text-dark ${styles.custom_option}`} value="2024">2024</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2023">2023</option>
+                                <option class={`text-dark ${styles.custom_option}`} value="2022">2022</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="results__hero-container">
+                    <div class="results__hero-image">
+                        <img alt="Misano MotoGP™ Official Test" src="https://photos.motogp.com/2024/events/background/13 RSM3.jpg" class="results__hero-image-img js-hero-image active"></img>
+                    </div>
+                    <div class="results__hero-text">Misano MotoGP™ Official Test 2024</div>
+                    <div class="results__hero-details-container">
+                        <img src="https://static-files.motogp.pulselive.com/assets/flags/it.svg" alt="IT flag" class="results__hero-details-flag"></img>
+                            <div class="results__hero-details-location">Misano World Circuit Marco Simoncelli</div><div class="results__hero-details-dates">09 Sep</div>
+                    </div>
+                </div>
+
+                <section class={`${styles.results_table}`}>
+                    <table class={`${styles.results_table__table}`}>
+                        <thead>
+                            <tr>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__pos}`}>pos.</th>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__rider}`}>rider</th>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__team}`}>Team</th>
+                                <th class={`${styles.results_table__header_cell} ${styles.results_table__header_cell__time}`}>point</th>
+                            </tr>
+                        </thead>
+                        <tbody class={`${styles.results_table__tbody}`}>
+                            <tr class={`${styles.results_table__body_row}`}>
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__pos} u-hide-tablet`}>1</td>
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__pos}`}>
+                                    <div class={`${styles.results_table__rider_details}`}>
+                                        <div class={`${styles.rider_image_container}`}>
+                                            <div class="rider-image">
+                                                <div class={`${styles.u_observed}`}>
+                                                    <img src="https://resources.motogp.pulselive.com/photo-resources/2024/02/19/986b0e12-1db0-49d8-ae13-fd556286237a/93_Marc_MarquezFullbodyGresini.png?height=300&amp;width=200"
+                                                        alt="rider-bio_marcmarquez" loading="lazy"
+                                                        class="img undefined picture__img "></img>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class={`${styles.results_table__rider_name_wrapper} u-hide-tablet`}>
+                                            <div class={`${styles.results_table__rider_name}`}>
+                                                <span class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__number}`}>93</span>
+                                                <span class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__full_name}`}>
+                                                    <a href="/en/riders/marc-marquez/23e50438-a657-4fb0-a190-3262b5472f29"
+                                                        title="label.standings.viewRiderProfile" class={`${styles.results_table__rider_link}`}>
+                                                        <span class={`${styles.results_table__first_name}`}>M. </span> Marquez</a></span>
+                                            </div>
+                                            <img src="https://static-files.motogp.pulselive.com/assets/flags/es.svg" alt="ES flag"
+                                                class={`${styles.results_table__body_cell_flag}`}></img>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__team} u-hide-tablet`}>Gresini Racing
+                                    MotoGP
+                                </td>
+                                <td class={`${styles.results_table__body_cell} ${styles.results_table__body_cell__point}`}><span>312</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
             </div>
-            <div class="primary-filter__filter-container">
-                <div class="primary-filter__filter-label">Championship</div>
-                <select class="primary-filter__filter-select primary-filter__filter-select--year">
-                    <option value="2024">2024</option>
-                    <option value="2023">2023</option>
-                </select>
-            </div>
-            <div class="primary-filter__filter-container">
-                <div class="primary-filter__filter-label">Category</div>
-                <select class="primary-filter__filter-select primary-filter__filter-select--year">
-                    <option value="2024">2024</option>
-                    <option value="2023">2023</option>
-                </select>
-            </div>
-        </div>
+        </>
     )
 }
 export default Standing;
