360a7b036106c79f55e684b851df6f9f922ef036
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2024-09-22 01:23:23 +0700

    sửa slidebar admin

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -6,18 +6,33 @@ import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import './admin.css';
 import SlideBar from './slidebar';
+
+import { Button, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Sidebar from './slidebar'; // Import the Sidebar component
 const Admin = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // State to manage sidebar visibility
+
+  // Function to toggle sidebar open
+  const handleSidebarOpen = () => {
+    setIsSidebarOpen(true);
+  };
+
+  // Function to close sidebar
+  const handleSidebarClose = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <div className="container-fluid min-vh-100">
-      <div className="row">
-        <div className="col-2 custom-sidebar min-vh-100">
-        </div>
+      <IconButton onClick={handleSidebarOpen} style={{ position: 'fixed', top: 70, left: 0 }}>
+        <MenuIcon />
+      </IconButton>
         {/* Main Content */}
         <div>
-          <SlideBar/>
-          <Outlet /> {/* Renders the nested routes */}
+          <SlideBar isOpen={isSidebarOpen} onClose={handleSidebarClose}/>
+          <Outlet />
         </div>
-      </div>
     </div>
 
   );
--- src/components/admin/slidebar.js
+++ src/components/admin/slidebar.js
@@ -8,49 +8,64 @@ import calend from './asset/calendarbw.png';
 import cup from './asset/cupbw.png';
 import logout from './asset/logoutbw.png';
 
-const SlideBar = () => {
+import { Drawer, IconButton, List, ListItem, ListItemText } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+const SlideBar = ({ isOpen, onClose }) => {
     return (
-        <div className="row">
-            <div className="col-2 custom-sidebar min-vh-100">
-            <div className="sidebar p-2">
-                <div className="m-2 d-flex align-items-center">
-                    <img src={adminLogo} alt="Admin Logo" style={{ width: '85px' }} />
-                    <span className="brand-name fs-4 ms-2">Admin</span>
-                </div>
+        <Drawer
+            variant="persistent"
+            anchor="left"
+            open={isOpen}
+            sx={{ width: 240, flexShrink: 0 }}
+        >
+            <div className="row">
+                <div className="col-2 custom-sidebar min-vh-100">
+                    <div style={{ display: 'flex', justifyContent: 'flex-end', padding: 8 }}>
+                        <IconButton onClick={onClose}>
+                            <CloseIcon />
+                        </IconButton>
+                    </div>
+                    <div className="sidebar p-2">
+                        <div className="m-2 d-flex align-items-center">
+                            <img src={adminLogo} alt="Admin Logo" style={{ width: '85px' }} />
+                            <span className="brand-name fs-4 ms-2">Admin</span>
+                        </div>
 
-                <hr className="text-light" />
+                        <hr className="text-light" />
 
-                {/* Sidebar Items */}
-                <div className="list-group list-group-flush">
-                    <Link to="account" className="list-group-item py-2 my-1 custom-sidebar-item">
-                        <img src={accLogo} alt="account logo" style={{ width: '35px' }} />
-                        <span className="fs-5 ms-2">Account</span>
-                    </Link>
+                        {/* Sidebar Items */}
+                        <div className="list-group list-group-flush">
+                            <Link to="account" className="list-group-item py-2 my-1 custom-sidebar-item">
+                                <img src={accLogo} alt="account logo" style={{ width: '35px' }} />
+                                <span className="fs-5 ms-2">Account</span>
+                            </Link>
 
-                    <Link to="/admin/racers-and-team" className="list-group-item py-2 my-1 custom-sidebar-item">
-                        <img src={helmet} alt="helmet logo" style={{ width: '27px' }} />
-                        <span className="fs-5 ms-2">Racers and team</span>
-                    </Link>
+                            <Link to="/admin/racers-and-team" className="list-group-item py-2 my-1 custom-sidebar-item">
+                                <img src={helmet} alt="helmet logo" style={{ width: '27px' }} />
+                                <span className="fs-5 ms-2">Racers and team</span>
+                            </Link>
 
-                    <Link to="/admin/calendaradmin" className="list-group-item py-2 custom-sidebar-item">
-                        <img src={calend} alt="calendar logo" style={{ width: '30px' }} />
-                        <span className="fs-5 ms-2">Calendar</span>
-                    </Link>
+                            <Link to="/admin/calendaradmin" className="list-group-item py-2 custom-sidebar-item">
+                                <img src={calend} alt="calendar logo" style={{ width: '30px' }} />
+                                <span className="fs-5 ms-2">Calendar</span>
+                            </Link>
 
-                    <Link to="/admin/ranking" className="list-group-item py-2 custom-sidebar-item">
-                        <img src={cup} alt="ranking logo" style={{ width: '30px' }} />
-                        <span className="fs-5 ms-2">Ranking</span>
-                    </Link>
+                            <Link to="/admin/ranking" className="list-group-item py-2 custom-sidebar-item">
+                                <img src={cup} alt="ranking logo" style={{ width: '30px' }} />
+                                <span className="fs-5 ms-2">Ranking</span>
+                            </Link>
 
-                    <Link to="/admin/logout" className="list-group-item py-2 custom-sidebar-item">
-                        <img src={logout} alt="logout logo" style={{ width: '30px' }} />
-                        <span className="fs-5 ms-2">Logout</span>
-                    </Link>
+                            <Link to="/admin/logout" className="list-group-item py-2 custom-sidebar-item">
+                                <img src={logout} alt="logout logo" style={{ width: '30px' }} />
+                                <span className="fs-5 ms-2">Logout</span>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
-            );
+        </Drawer>
+    );
 };
 
 export default SlideBar;
